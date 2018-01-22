b4aacec0cd41fd05a28b850a32da2fd430f38794
ism8531694 <ism8531694@a21.informatica.escoladeltreball.org>
2018-01-22 13:51:22 +0100

    First load

---
+++ vocales.py
@@ -0,0 +1,11 @@
+#coding: utf8
+letras = raw_input ("pon alguna letra:	")
+if letras == 'a' or letras == 'e' or letras == 'i' or letras == 'o' or letras == 'u':
+	print "es vocal"
+else:
+	print "no es vocal"  
+
+if letras == 'q' or letras == 'w' or letras == 'r' or letras == 't' or letras == 'y' or letras == 'p' or letras == 's' or letras == 'd' or letras == 'f' or letras == 'g' or letras == 'h' or letras == 'j' or letras == 'k' or letras == 'l' or letras == 'ñ' or letras == 'ç' or letras == 'z' or letras == 'x' or letras == 'c' or letras == 'v' or letras == 'b' or letras == 'n' or letras == 'm':
+	print "sino consonante"
+else:
+	print "no es consonate"
